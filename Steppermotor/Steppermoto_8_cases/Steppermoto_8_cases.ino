63044588f00c17fbc2b17b0ceed89e8a8d535559
Iris Edelsmid <irisedelsmid@gmail.com>
2017-03-03 17:58:13 +0100

    with 8 cases to make it a stronger motor test

---
+++ Steppermotor/Steppermoto_8_cases/Steppermoto_8_cases.ino
@@ -0,0 +1,81 @@
+//this is a test with 8 cases to make the steppermotor stronger. 
+int bluePin = 2;
+int pinkPin = 3;
+int yellowPin = 4;
+int orangePin = 5;
+int currentStep = 0;
+
+int waveStepCount = 4;
+int totalCases = 8;
+
+bool clockwise = false;
+
+void setup() {
+pinMode(bluePin, OUTPUT);
+pinMode(pinkPin, OUTPUT);
+pinMode(yellowPin, OUTPUT);
+pinMode(orangePin, OUTPUT);
+
+digitalWrite(bluePin, LOW);
+digitalWrite(pinkPin, LOW);
+digitalWrite(yellowPin, LOW);
+digitalWrite(orangePin, LOW);
+}
+void loop() {
+
+  int directionStep = clockwise ? currentStep : (totalCases-1)-currentStep;
+switch(directionStep){
+  case 0:
+    digitalWrite(bluePin, HIGH);
+    digitalWrite(pinkPin, LOW);
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(orangePin, LOW);
+    break;
+  case 1:
+    digitalWrite(bluePin, HIGH);
+    digitalWrite(pinkPin, HIGH);
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(orangePin, LOW);
+    break;
+  case 2:
+    digitalWrite(bluePin, LOW);
+    digitalWrite(pinkPin, HIGH);
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(orangePin, LOW);
+    break;
+  case 3:
+    digitalWrite(bluePin, LOW);
+    digitalWrite(pinkPin, HIGH);
+    digitalWrite(yellowPin, HIGH);
+    digitalWrite(orangePin, LOW);
+    break;
+  case 4:
+    digitalWrite(bluePin, LOW);
+    digitalWrite(pinkPin, LOW);
+    digitalWrite(yellowPin, HIGH);
+    digitalWrite(orangePin, LOW);
+    break;
+  case 5:
+    digitalWrite(bluePin, LOW);
+    digitalWrite(pinkPin, LOW);
+    digitalWrite(yellowPin, HIGH);
+    digitalWrite(orangePin, HIGH);
+    break;
+  case 6:
+    digitalWrite(bluePin, LOW);
+    digitalWrite(pinkPin, LOW);
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(orangePin, HIGH);
+    break;
+  case 7:
+    digitalWrite(bluePin, HIGH);
+    digitalWrite(pinkPin, LOW);
+    digitalWrite(yellowPin, LOW);
+    digitalWrite(orangePin, HIGH);
+    break;
+}
+currentStep = (++currentStep <totalCases ) ? currentStep : 0;
+
+
+delay(2);
+}
