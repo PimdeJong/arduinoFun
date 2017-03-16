1e16b4c7363befb82c8f301f57f29a6a95e777a8
Iris Edelsmid <irisedelsmid@gmail.com>
2017-03-16 23:00:43 +0100

    Opzet claw voorwaarden

--- Claw/Claw_controller/Claw_controller.ino
+++ Claw/Claw_controller/Claw_controller.ino
@@ -1,19 +1,25 @@
-int buttonState = 1 //
-                  int servoPosition
+int buttonState = 1 ;//
+int servoPosition;
+int grabCount;
+int teller;
+int vorigeStatus;
 
-                  //this is nothing yet, working on it. ;)
-                  /*
-                    Een grijper die open of dicht gaat naar aanleiding van de waarde die middels een draaiknop wordt gegeven (en middels MAP functie omgevormd tot een bruikbare waarde)
-                    Als een object gegrepen is,
-                    dan luidt er een buzzer.
-                    En
-                    Het ledje gaat knipperen
-                    Als hoe kleiner de hoek is van de grijper, hoe feller het ledje gaat branden
+bool grabdingStaatStil = false;
+bool claw = false;
 
-                    Op de LCD wordt weergeven:
-                    de hoek van de motor(dus de grijper) = Vraagt de waarde op van de huidige grijperwaarde
-                    Hoe vaak een object is gepakt = Telt
-                    En geeft weer indien een object wordt vastgehouden.*/
+//this is nothing yet, working on it. ;)
+/*
+  Een grijper die open of dicht gaat naar aanleiding van de waarde die middels een draaiknop wordt gegeven (en middels MAP functie omgevormd tot een bruikbare waarde)
+  Als een object gegrepen is,
+  dan luidt er een buzzer.
+  En
+  Het ledje gaat knipperen
+  Als hoe kleiner de hoek is van de grijper, hoe feller het ledje gaat branden
+
+  Op de LCD wordt weergeven:
+  de hoek van de motor(dus de grijper) = Vraagt de waarde op van de huidige grijperwaarde
+  Hoe vaak een object is gepakt = Telt
+  En geeft weer indien een object wordt vastgehouden.*/
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,6 +29,36 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  analogRead (buttonState)
-  servoposition = map(buttonState, 0, 1023, 0, 180) //" map(value, fromLow, fromHigh, toLow, toHigh)
+  analogRead (buttonState);
+  servoposition = map(buttonState, 0, 1023, 0, 180); //" map(value, fromLow, fromHigh, toLow, toHigh)
+
+  if (graden = < 30 && grabdingStaatStil)
+  {
+    if (claw == false)
+    {
+      claw = true;
+      grabCount ++;
+    }
+  }
+  else
+  {
+    claw = false;
+  }
+
+
+  
+
+  if (buttonState = vorigeStatus )
+  {
+    teller ++;
+  }
+
+  if (teller >= 30)
+  {
+    grabdingStaatStil = true;
+
+  }
+  vorigeStatus = buttonState;
+
+
 }
