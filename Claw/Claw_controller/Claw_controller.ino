d75be5907574117173ba4343af56868fc3743ec4
Iris Edelsmid <irisedelsmid@gmail.com>
2017-03-19 18:02:56 +0100

    You get one piece of paper to describe your past

--- Claw/Claw_controller/Claw_controller.ino
+++ Claw/Claw_controller/Claw_controller.ino
@@ -2,50 +2,32 @@
   { public: int angle;
   public: int aantalKeerStilStaan;
   virtual bool StaatStil(){   }
-  };*/
-
+  };*/                                    // ignore this I'm just playing with objects
 int _buttonState = 1 ;//
 int _servoPosition;
 int _grabCount;
 int _teller;
 int _vorigeStatus;
 int _angle; //????
-
 bool _grabdingStaatStil = false;
 bool _claw = false;
-
 //led setup:
 int _ledPin = 9;                            // LED connected to digital pin 9
 bool _blinkState = false;
 unsigned long _time;
 unsigned long _time2 = 0 ;
+/*  Een grijper die open of dicht gaat naar aanleiding van de waarde die middels een draaiknop wordt gegeven (en middels MAP functie omgevormd tot een bruikbare waarde)
+    Als een object gegrepen is:   dan luidt er een buzzer.    En  Het ledje gaat knipperen        Hoe kleiner de hoek is van de grijper, hoe feller het ledje gaat branden
+    Op de LCD wordt weergeven:
+    de hoek van de motor(dus de grijper) = Vraagt de waarde op van de huidige grijperwaarde       Hoe vaak een object is gepakt = Telt        En geeft weer indien een object wordt vastgehouden.*/
+void setup() {}
 
-/*
-  Een grijper die open of dicht gaat naar aanleiding van de waarde die middels een draaiknop wordt gegeven (en middels MAP functie omgevormd tot een bruikbare waarde)
-  Als een object gegrepen is,
-  dan luidt er een buzzer.
-  En
-  Het ledje gaat knipperen
-  Als hoe kleiner de hoek is van de grijper, hoe feller het ledje gaat branden
-
-  Op de LCD wordt weergeven:
-  de hoek van de motor(dus de grijper) = Vraagt de waarde op van de huidige grijperwaarde
-  Hoe vaak een object is gepakt = Telt
-  En geeft weer indien een object wordt vastgehouden.*/
-
-void setup() {
-}
-
-void loop() 
+void loop()
 {
   analogRead (_buttonState);
-  _servoPosition = map(_buttonState, 0, 1023, 0, 180); //" map(value, fromLow, fromHigh, toLow, toHigh)
+  _servoPosition = map(_buttonState, 0, 1023, 0, 180);                            /*" funtie: map(value, fromLow, fromHigh, toLow, toHigh)*/
   _claw = Claw (_angle, _grabdingStaatStil, _claw, _grabCount);
-  /*ClawClass *clawInstantie = new ClawClass();*/ // = spelen met objecten
-  analogWrite (_ledPin, LedLight()); /*Ledlight is een apparte methode ->> + () anders weet m computertje niet dat het om een methode gaat*/
-  _grabdingStaatStil = ClawStagnation (_teller, _buttonState, _vorigeStatus);  /*om te zorgen dat hij herkent dat de motor stilstaat*/
+  /*ClawClass *clawInstantie = new ClawClass();*/                                 // = spelen met objecten
+  analogWrite (_ledPin, LedLight());                                              /*Ledlight is een apparte methode ->> + () anders weet m computertje niet dat het om een methode gaat*/
+  _grabdingStaatStil = ClawStagnation (_teller, _buttonState, _vorigeStatus);     /*om te zorgen dat hij herkent dat de motor stilstaat*/
 }
-
-
-
-
