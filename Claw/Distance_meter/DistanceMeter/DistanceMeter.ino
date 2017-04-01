26d1bc1b05bbc70cda76a4dee62e2e4f09439c11
Iris Edelsmid <irisedelsmid@gmail.com>
2017-04-01 21:00:12 +0200

    CleanUpYourRoomMistaLover

--- Claw/Distance_meter/DistanceMeter/DistanceMeter.ino
+++ Claw/Distance_meter/DistanceMeter/DistanceMeter.ino
@@ -5,55 +5,66 @@ Servo servo;
 // consisent
 const int buttonPin = 2;
 const int trigPin = 6;
-const int echoPin = 7;    
-const int servoPin = 13;       
- 
+const int echoPin = 7;
+const int servoPin = 13;
+
 // Variable
 int buttonPushCounter = 0;   // counter for the number of button presses
 int buttonState = 0;         // current state of the button
 int lastButtonState = 0;     // previous state of the button
-int pos = 0;  
+int pos = 0;
 
 void setup () {
   pinMode(buttonPin, INPUT);
   pinMode(servoPin, OUTPUT);
-  servo.attach(9); 
-  Serial.begin(9600); }
- 
+  servo.attach(9);
+  Serial.begin(9600);
+}
+
 void loop() {
-long duration, cm;
-pinMode(trigPin, OUTPUT);
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-pinMode(echoPin, INPUT);
-duration = pulseIn(echoPin, HIGH);
- 
-cm = microsecondsToCentimeters(duration);
-Serial.print(cm);
-Serial.print("cm");
-Serial.println();
+  long duration, cm;
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
 
-buttonState = digitalRead(buttonPin);
- if (buttonState != lastButtonState) {
-  if (buttonState == HIGH) {
-   buttonPushCounter++; }
-   delay(50);  }
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
 
-//servo 
-if (cm <= 10) {sweepforward(3); }
- else if (cm >= 24) {servo.write(pos); }
-  delay(100); }
- 
+  buttonState = digitalRead(buttonPin);
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+      buttonPushCounter++;
+    }
+    delay(50);
+  }
+
+  //servo
+  if (cm <= 10) {
+    sweepforward(3);
+  }
+  else if (cm >= 24) {
+    servo.write(pos);
+  }
+  delay(100);
+}
+
 long microsecondsToCentimeters(long microseconds)
-{ return microseconds / 29 / 2; }
+{
+  return microseconds / 29 / 2;
+}
 
 //voidsweep moet nog aangeroepen worden
 void sweepforward(int NUM_OF_CYCLES) {
- for (int j=1; j<NUM_OF_CYCLES; j++)
- for(pos = 0; pos = 170; pos += 1) 
- { servo.write(pos);
-   delay(100); } 
+  for (int j = 1; j < NUM_OF_CYCLES; j++)
+    for (pos = 0; pos = 170; pos += 1)
+    { servo.write(pos);
+      delay(100);
+    }
 }
