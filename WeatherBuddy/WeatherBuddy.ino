4b52b0464d6be4273856b72cabe91a8535d256e7
Iris Edelsmid <irisedelsmid@gmail.com>
2017-05-30 15:46:52 +0200

    Raindanse because it's working <3

--- WeatherBuddy/WeatherBuddy.ino
+++ WeatherBuddy/WeatherBuddy.ino
@@ -1,48 +1,46 @@
-const int pinSwitch = 12;  //Pin Reed
-const int pinLed    = 10;  //Pin LED
-int REGENSENSOR = A1; // Regensensor aan analoge ingang 1
-int LED = 9; // LED aan pen 8 gaat aan als het sneeuwt of refent
+// Code Bas, 
 
-int val = 0;
-int data = 0;
-
-
+int pinSwitch = 12;  //Pin Reed
+int regenSensor = A1; // Regensensor aan analoge ingang 1, als regensor dan gaat led 9 (rood) branden
+int LED = 9; // LED aan pen 9 gaat aan als het sneeuwt of refent
+int reedLed = 10; // LED a
+int val = 0;  // = dus de regensensorwaarde
 int StatoSwitch = 0;
+
 
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(pinLed, OUTPUT);
+  Serial.begin(115200);//Serial.begin(9600);
+  pinMode(reedLed, OUTPUT);
   pinMode(pinSwitch, INPUT);
   pinMode(LED, OUTPUT);
 
 
 }
 void loop() {
-  val = analogRead(REGENSENSOR);
+  Serial.print("hoi");
+  val = analogRead(regenSensor);
   if (val > 700) {
     digitalWrite(LED, LOW);
   }
   else {
     digitalWrite(LED, HIGH);
   }
-  data = val;
   Serial.print("Neerslaghoeveelheid: " );
-  Serial.print(data);
+  Serial.print(val);
   delay(400);
 
-
-
+  
   StatoSwitch = digitalRead(pinSwitch);
   if (StatoSwitch == HIGH)
   {
-    Serial.println("Magnet near Reed Switch detected!");
-    digitalWrite(pinLed, HIGH);
+    Serial.println(" Magnet near Reed Switch detected! ");
+    digitalWrite(reedLed, HIGH);
   }
   else
   {
-    Serial.println("No Magnet Detected");
-    digitalWrite(pinLed, LOW);
+    Serial.println(" No Magnet Detected ");
+    digitalWrite(reedLed, LOW);
   }
 
 }
