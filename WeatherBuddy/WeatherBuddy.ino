32077ae0eee5b6d3b35b7370ad5d5f8c3432305c
Iris Edelsmid <irisedelsmid@gmail.com>
2017-05-30 16:28:25 +0200

    Hiep hoi Reedsensor

--- WeatherBuddy/WeatherBuddy.ino
+++ WeatherBuddy/WeatherBuddy.ino
@@ -1,12 +1,12 @@
-// Code Bas, 
+// start met code van Bas die niet werkte, uplouds github door mij (Iris), Beetje vieze code nog maar het weerstation staat ;)
 
 int pinSwitch = 12;  //Pin Reed
 int regenSensor = A1; // Regensensor aan analoge ingang 1, als regensor dan gaat led 9 (rood) branden
 int LED = 9; // LED aan pen 9 gaat aan als het sneeuwt of refent
-int reedLed = 10; // LED a
+int reedLed = 10; // LED voor de reed, groen
 int val = 0;  // = dus de regensensorwaarde
 int StatoSwitch = 0;
- // andere ledje direct in 5V geeft aan of hij aan is (rood)
+// andere ledje direct in 5V geeft aan of hij aan is (rood)
 
 
 void setup()
@@ -15,12 +15,12 @@ void setup()
   pinMode(reedLed, OUTPUT);
   pinMode(pinSwitch, INPUT);
   pinMode(LED, OUTPUT);
-
-
 }
+
 void loop() {
   val = analogRead(regenSensor);
-  if (val > 700) {
+  if (val > 700) 
+  {
     digitalWrite(LED, LOW);
   }
   else {
@@ -30,7 +30,7 @@ void loop() {
   Serial.print(val);
   delay(400);
 
-  
+
   StatoSwitch = digitalRead(pinSwitch);
   if (StatoSwitch == HIGH)
   {
