97a90ff713970b391be96f4201e3a20c0542b0d8
Iris Edelsmid <irisedelsmid@gmail.com>
2017-06-10 19:05:13 +0200

    Dont lie to me about your true maximum

---
+++ Buddy/Iris_buddy_workFile/Calibration.ino
@@ -0,0 +1,11 @@
+void calibrate() {
+  _infraredMin = 1023;
+  _infraredMax = 0;
+  unsigned long timestamp = millis();
+  while (millis() - timestamp < 5000) {
+    value = (analogRead(0) + analogRead(0) + analogRead(0) + analogRead(0)) / 4;
+    _infraredMin = min(_infraredMin, value);
+    _infraredMax = max(_infraredMax, value);
+  }
+}
+
