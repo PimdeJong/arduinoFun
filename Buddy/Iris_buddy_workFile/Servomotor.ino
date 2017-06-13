685b803635d473b1ccad1992379a8d3c0353a0e0
Iris Edelsmid <irisedelsmid@gmail.com>
2017-06-13 23:52:55 +0200

    Mouse chocolato di Salvador Dali

--- Buddy/Iris_buddy_workFile/Servomotor.ino
+++ Buddy/Iris_buddy_workFile/Servomotor.ino
@@ -5,9 +5,9 @@ void HiepHoiServo()
   _infraredArea = _infraredMax - _infraredMin;
   int infraredServo = _infrared;
   int infraredServoMin = _infraredMin;
-  infraredServoMin = (infraredServoMin + (_infraredArea * 0, 05));
+  infraredServoMin = (infraredServoMin + (_infraredArea * 0, 01));
   int infraredServoMax = _infraredMax;
-  infraredServoMax = infraredServoMax - (_infraredArea * 0, 05);
+  infraredServoMax = infraredServoMax - (_infraredArea * 0, 02);
 
   if (infraredServo <= infraredServoMin)
   {
@@ -18,7 +18,7 @@ void HiepHoiServo()
     infraredServo = infraredServoMax;
   }
 
-  servoStand = map(infraredServo, infraredServoMin, infraredServoMax, 0, 180); // scale it to use it with the servo (value between 0 and 180)
+  servoStand = map(infraredServo, infraredServoMax,infraredServoMin, 0, 180); // scale it to use it with the servo (value between 0 and 180)
 
   Serial.println("servostand " + String(servoStand));
   Serial.println("ServoMin: " + String(infraredServoMin));
