5e1e21db93ca547d3764f023cc43e36c38c615bf
PimdeJong <38374981+PimdeJong@users.noreply.github.com>
2018-04-14 11:58:00 +0200

    Add files via upload

---
+++ Time machine countdown and crash with Dotmatrixes/testje/_180329_test/_180329_test.ino
@@ -0,0 +1,148 @@
+//We always have to include the library
+#include "LedControl.h"
+//include this file so we can write down a byte in binary encoding
+//#include <binary.h>
+
+/*
+  Now we need a LedControl to work with.
+  pin 12 is connected to the DataIn
+  pin 11 is connected to the CLK
+  pin 10 is connected to LOAD
+  We have only a single MAX72XX.
+*/
+byte aantal = 8;
+LedControl lc = LedControl(12, 11, 10, aantal);
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime = 100;
+
+void setup() {
+  delay(100);
+  Serial.begin(9600);
+  /*
+    The MAX72XX is in power-saving mode on startup,
+    we have to do a wakeup call
+  */
+  for (int i = 0; i < aantal; i++) {
+    lc.shutdown(i, false);
+
+    /* Set the brightness to a medium values */
+    lc.setIntensity(i, 2);
+
+    /* and clear the display */
+    lc.clearDisplay(i);
+  }
+}
+
+byte NUMBER [][8] = {
+  {
+    B00010000,
+    B00110000,
+    B00010000,
+    B00010000,
+    B00010000,
+    B00010000,
+    B00010000,
+    B00111000
+  }, {
+    B00111000,
+    B01000100,
+    B00000100,
+    B00000100,
+    B00001000,
+    B00010000,
+    B00100000,
+    B01111100
+  }, {
+    B00111000,
+    B01000100,
+    B00000100,
+    B00011000,
+    B00000100,
+    B00000100,
+    B01000100,
+    B00111000
+  }, {
+    B00000100,
+    B00001100,
+    B00010100,
+    B00100100,
+    B01000100,
+    B01110100,
+    B00000100,
+    B00000100
+  }, {
+    B01111100,
+    B01000000,
+    B01000000,
+    B01111000,
+    B00000100,
+    B00000100,
+    B01000100,
+    B00111000
+  }, {
+    B00111000,
+    B01000100,
+    B01000000,
+    B01111000,
+    B01000100,
+    B01000100,
+    B01000100,
+    B00111000
+  }, {
+    B01111100,
+    B00000100,
+    B00000100,
+    B00001000,
+    B00010000,
+    B00100000,
+    B00100000,
+    B00100000
+  }, {
+    B00111000,
+    B01000100,
+    B01000100,
+    B00111000,
+    B01000100,
+    B01000100,
+    B01000100,
+    B00111000
+  }, {
+    B00111000,
+    B01000100,
+    B01000100,
+    B01000100,
+    B00111100,
+    B00000100,
+    B01000100,
+    B00111000
+  }, {
+    B00111000,
+    B01000100,
+    B01000100,
+    B01000100,
+    B01000100,
+    B01000100,
+    B01000100,
+    B00111000
+  }
+};
+int c = 0;
+
+void loop() {
+
+  for (int j = 0; j < 10; j ++) {
+    //  long j = random(10);
+    for (int i = 0; i < 8; i ++) {
+      for (int c = 0; c < aantal; c++) {
+        lc.setRow(c, i, NUMBER [j] [i]);
+      }
+    }
+    delay(2000);
+    for ( int i = 0; i < aantal; i ++) {
+      lc.clearDisplay(i);
+    }
+  }
+  // lc.setColumn(0,0,B10101010);
+  // delay(2000);
+}
---
+++ Time machine countdown and crash with Dotmatrixes/testje/random/random.ino
@@ -0,0 +1,31 @@
+#include "LedControl.h"
+byte aantal = 8;
+LedControl lc = LedControl(12, 11, 10, aantal);
+unsigned long delaytime = random (0, 100);
+
+void setup() {
+  delay(100);
+  Serial.begin(9600);
+  /*
+    The MAX72XX is in power-saving mode on startup,
+    we have to do a wakeup call
+  */
+  for (int i = 0; i < aantal; i++) {
+    lc.shutdown(i, false);
+
+    /* Set the brightness to a medium values */
+    lc.setIntensity(i, 2);
+
+    /* and clear the display */
+    lc.clearDisplay(i);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int  l =  random(0, 8);
+int  c = random (0, 8);
+int  r = random (0, 8);
+ lc.setLed(l, r, c, true);
+delay (delaytime);
+}
